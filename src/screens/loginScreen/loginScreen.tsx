95738a0888d10bb808ac5960a27e5cf47879aae2
supolaris <msulaman061@gmail.com>
2023-04-17 23:27:07 +0500

    update
    login UI almost done

--- src/screens/loginScreen/loginScreen.tsx
+++ src/screens/loginScreen/loginScreen.tsx
@@ -1,20 +1,81 @@
 import React from "react";
-import {View, Text, Image} from "react-native";
+import {View, Text, Image, SafeAreaView, TextInput, TouchableOpacity, Alert} from "react-native";
 import loginLogo from "../../../assets/login.png";
+import facebook from "../../../assets/Facebook.png";
+import google from "../../../assets/Google.png";
+import apple from "../../../assets/Apple.png";
 import { loginStyles } from "./loginStyles";
-
+import MaterialIcons from "react-native-vector-icons/MaterialIcons";
+import Feather from "react-native-vector-icons/Feather";
 
 
 export default function Login () {
 
     return(
-        <View style={loginStyles.container}>
-            <Image
-            style={loginStyles.image}
-            source={loginLogo}
-            />
-            
-            <Text>This is login Screen</Text>
-        </View>
+        <SafeAreaView style={loginStyles.container}>
+            <View style={{paddingHorizontal: 20}}>
+                <View style={loginStyles.imageContainer}>
+                    <Image
+                    style={loginStyles.image}
+                    source={loginLogo}
+                    />
+                </View>
+                <Text style={loginStyles.text}>Login</Text>
+                
+                <View style={loginStyles.icon_textinput}>
+                    <MaterialIcons 
+                    style={loginStyles.icon}
+                    name="alternate-email" size={25} color="#666"/>
+                    <TextInput
+                    style={loginStyles.input}
+                    placeholder="Enter Email"
+                    keyboardType="email-address"
+                    />
+                </View>
+                <View style={loginStyles.icon_textinput}>
+                    <Feather 
+                    style={loginStyles.icon}
+                    name="lock" size={25} color="#666"/>
+                    <TextInput
+                    style={loginStyles.input}
+                    placeholder="Enter password"
+                    secureTextEntry={true}
+                    />
+                    <TouchableOpacity 
+                    style={loginStyles.forgotTouch}
+                    onPress={() => {Alert.alert("Warning" , "Why you forgot your password")}}
+                    >
+                        <Text style={loginStyles.forgotText}>Forgot ?</Text>
+                    </TouchableOpacity>
+                </View>
+                <TouchableOpacity style={loginStyles.buttonTouch}>
+                    <Text style={loginStyles.buttonText}>Login</Text>
+                </TouchableOpacity>
+                <Text style={loginStyles.text2} >Or login with ...</Text>
+
+                <View style={{flexDirection: 'row', justifyContent: 'space-around', marginBottom: 20}}>
+                    <TouchableOpacity style={{borderWidth: 1, borderRadius: 5, paddingVertical: 10, paddingHorizontal: 20}}>
+                        <Image 
+                        style={loginStyles.cLogo}
+                        source={facebook}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{borderWidth: 1, borderRadius: 5, paddingVertical: 10, paddingHorizontal: 20}}>
+                        <Image 
+                        style={loginStyles.cLogo}
+                        source={google}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{borderWidth: 1, borderRadius: 5, paddingVertical: 10, paddingHorizontal: 20}}>
+                        <Image
+                        style={loginStyles.cLogo} 
+                        source={apple}
+                        />
+                    </TouchableOpacity>
+                </View>
+                <Text style={loginStyles.text3}>New to App? <Text style={loginStyles.innerText}>Rgister</Text></Text>
+                
+            </View>
+            </SafeAreaView>
     );
 }
--- src/screens/loginScreen/loginStyles.tsx
+++ src/screens/loginScreen/loginStyles.tsx
@@ -4,12 +4,88 @@ import { StyleSheet } from "react-native";
 export const loginStyles = StyleSheet.create({
     container: {
         flex: 1,
-      //  justifyContent: 'center',
+        justifyContent: 'center',
 
     }, 
-    image: {
-        height: 200,
-        width: 300,
-        //alignContent: 'center'
+    imageContainer: {
+        alignItems: 'center',
     },
+    image: {
+        height: 300,
+        width: 300,
+       // transform: '-10deg'
+        //alignContent: 'center'
+        
+    },
+    text: {
+        fontSize: 28,
+        fontWeight: '600',
+        color: 'black',
+        marginBottom: 20,
+    },
+    icon_textinput: {
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        paddingBottom: 5,
+        marginBottom: 25,
+        
+
+    },
+    icon: {
+        marginRight: 5,
+
+    },
+    input: {
+        flex: 1,
+        paddingVertical: 0,
+        fontSize: 18,
+        
+
+    },
+    forgotTouch: {
+
+    },
+    forgotText: {
+        fontSize: 16,
+        color: '#ad40af',
+        fontWeight: '700',
+
+    },
+    buttonTouch: {
+        padding: 20,
+        backgroundColor: '#ad40af',
+        borderRadius: 10,
+        marginBottom: 20,
+
+    },
+    text2: {
+        fontSize: 16,
+        textAlign: 'center',
+        fontWeight: '700',
+        marginBottom: 20,
+    },
+    buttonText: {
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: '700',
+        color: '#ffffff'
+    },
+
+    cLogo: {
+        height: 25,
+        width: 25,
+        padding: 10,
+    },
+    text3: {
+        fontSize: 18,
+        textAlign: 'center',
+        fontWeight: '500',
+        marginBottom: 60,
+        color: 'black',
+        
+
+    },
+    innerText: {
+        color: '#ad40af',
+    }
 })
